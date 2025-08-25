36a0c22a0f8f170a54ae4f2ba13b50e4dce86c1c
amayones <mayonedrive1@gmail.com>
2025-08-25 09:47:00 +0700

    feat: predict service implementation

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const Handler = require('./handler');
+const PredictService = require('./predict-service');
 
 const init = async () => {
   const server = Hapi.server({
@@ -8,6 +9,7 @@ const init = async () => {
     host: '0.0.0.0',
   });
 
+  const predictService = new PredictService();
   const handler = new Handler(predictService);
   server.route(routes(handler));
 
